42186aa6537d6f38a0553ee21d8d6f0948f497bb
Caleb Cohen <calebcohen@hotmail.com>
2022-08-10 22:34:40 -0500

    edit feature

--- server.js
+++ server.js
@@ -49,5 +49,31 @@ db.on('error', console.error.bind(console, 'connection error:'));
         }
     })
 
+    //EDIT or UPDATE METHOD
+    app
+        .route("/edit/:id")
+        .get((req, res) => {
+            const id = req.params.id
+            TodoTask.find({}, (err,tasks) => {
+                res.render('edit.ejs', {
+                    todoTasks:tasks, idTask: id });
+                });
+            })
+        .post((req, res) => {
+            const id = req.params.id
+            TodoTask.findByIdAndUpdate(
+                id,
+                {
+                    title: req.body.title,
+                    content: req.body.content
+                },
+                err => {
+                    if (err) return res.status(500).send(err)
+                    res.redirect('/')
+                }
+            )
+        })
+
+
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
